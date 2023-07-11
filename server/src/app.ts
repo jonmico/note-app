b0ec52c61df9f09d5739bce60c0901d61ce4616d
Jon Mico <jonmico@gmail.com>
2023-07-11 16:09:30 -0400

    Add HTTP error handling.

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import notesRoutes from './routes/notes';
 import morgan from 'morgan';
+import createHttpError, { isHttpError } from 'http-errors';
 
 const app = express();
 
@@ -12,15 +13,20 @@ app.use(express.json());
 app.use('/api/notes', notesRoutes);
 
 app.use((req, res, next) => {
-  next(Error('Endpoint not found'));
+  next(createHttpError(404, 'Endpoint not found'));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   let errorMessage = 'An unknown error occurred';
-  if (err instanceof Error) errorMessage = err.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+
+  if (isHttpError(err)) {
+    statusCode = err.status;
+    errorMessage = err.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
