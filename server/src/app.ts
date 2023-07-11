c20b7f7b67eb12cdc8d423b8b45f7d37100f59d9
Jon Mico <jonmico@gmail.com>
2023-07-11 14:39:37 -0400

    Add error handling.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,29 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import NoteModel from './models/note';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/', async (req, res, next) => {
+  try {
+    // throw Error('oh no!');
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error('Endpoint not found'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  let errorMessage = 'An unknown error occurred';
+  if (err instanceof Error) errorMessage = err.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
